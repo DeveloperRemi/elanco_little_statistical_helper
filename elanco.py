906b25e137e771f2e7eb8789a5c9f26fbc844481
DeveloperRemi <78056312+DeveloperRemi@users.noreply.github.com>
2021-01-31 01:37:32 +0000

    Add files via upload

---
+++ elanco.py
@@ -0,0 +1,124 @@
+from methods import methodsss as mthds
+import requests
+import json
+from tkinter import *
+
+#initialization of output box
+master = Tk()
+master.title('ELANCO LITTLE STATISTICAL HELPER')
+master.geometry("670x750")
+
+#publicly accessible variable
+labels = []
+
+#crearing output box method
+def clear():
+		for label in labels:
+			label.destroy()
+
+		del labels[:]
+
+#dropdown RESOURCES list
+OPTIONS = mthds.send_request_res()
+
+variable_res = StringVar(master)
+variable_res.set('Resources') # default value
+w = OptionMenu(master, variable_res, *OPTIONS)
+w.pack()
+w.place(relx=0.2, rely=0.01, anchor=N)
+
+#dropdown APPLICATIONS list
+OPTIONS2 = mthds.send_request_apps()
+
+variable_apps = StringVar(master)
+variable_apps.set('Applications') # default value
+v = OptionMenu(master, variable_apps, *OPTIONS2)
+v.pack()
+v.place(relx=0.53, rely=0.01, anchor=N)
+
+#dropdown TOPS list
+OPTIONS3 = mthds.top_tens
+
+variable_top_tens = StringVar(master)
+variable_top_tens.set('Top tens') # default value
+w = OptionMenu(master, variable_top_tens, *OPTIONS3)
+w.pack()
+w.place(relx=0.83, rely=0.01, anchor=N)
+
+
+
+
+# OK button
+# when OK is pressed VAR = pressed/selected value
+def ok():
+	latest_op_one = variable_res.get()
+	latest_op_two = variable_apps.get()
+	latest_op_three = variable_top_tens.get()
+	
+	#Check which of the dropdown lists were selected
+	#if RESOURCES list
+	if latest_op_one != "Resources":
+		var = latest_op_one
+		urrl = "https://engineering-task.elancoapps.com/api/resources/" + var
+		response_url = requests.get(urrl)
+		data = response_url.text
+		lst = json.loads(data)
+
+		if  len(labels) == 0:
+			my_label = Label(text=var + "\n\nTotal cost: " + str(mthds.total_cost(lst)) + "\nAverage cost: " + str(mthds.average_cost(lst)) + "\nData frame: " + str(mthds.time_frame(lst)[0]) + " - " + str(mthds.time_frame(lst)[1]) + "\nTop location: " + mthds.location_counter(lst))
+			my_label.pack(pady=(100,0), side=TOP, anchor=NW)
+		else:
+			my_label = Label(text=var + "\n\nTotal cost: " + str(mthds.total_cost(lst)) + "\nAverage cost: " + str(mthds.average_cost(lst)) + "\nData frame: " + str(mthds.time_frame(lst)[0]) + " - " + str(mthds.time_frame(lst)[1]) + "\nTop location: " + mthds.location_counter(lst))
+			my_label.pack(pady=24, side=TOP, anchor=NW)
+	
+	#if APPLICATIONS list
+	elif latest_op_two != "Applications":
+		var = latest_op_two
+		urrl = "https://engineering-task.elancoapps.com/api/applications/" + var
+		response_url = requests.get(urrl)
+		data = response_url.text
+		lst = json.loads(data)
+
+		if  len(labels) == 0:
+			my_label = Label(text=var + "\n\nTotal cost: " + str(mthds.total_cost(lst)) + "\nAverage cost: " + str(mthds.average_cost(lst)) + "\nData frame: " + str(mthds.time_frame(lst)[0]) + " - " + str(mthds.time_frame(lst)[1]) + "\nTop location: " + mthds.location_counter(lst))
+			my_label.pack(pady=(100,0), side=TOP, anchor=NW)
+		else:
+			my_label = Label(text=var + "\n\nTotal cost: " + str(mthds.total_cost(lst)) + "\nAverage cost: " + str(mthds.average_cost(lst)) + "\nData frame: " + str(mthds.time_frame(lst)[0]) + " - " + str(mthds.time_frame(lst)[1]) + "\nTop location: " + mthds.location_counter(lst))
+			my_label.pack(pady=24, side=TOP, anchor=NW)
+	
+	#if TOP TENS list
+	else:
+		var = latest_op_three
+		if  len(labels) == 0:			
+			my_label = Label(text=var +"\n\n" + mthds.which_top_selection(var))
+			my_label.pack(pady=(100,0), side=TOP, anchor=NW)
+		else:
+			my_label = Label(text=var +"\n\n" + mthds.which_top_selection(var))
+			my_label.pack(pady=24, side=TOP, anchor=NW)
+	
+
+	
+
+	#update publicly accesible list 'labels'
+	labels.append(my_label)
+
+	#set all dropdown lists to default position
+	variable_res.set('Resources')
+	variable_apps.set('Applications')
+	variable_top_tens.set('Top tens')
+	
+	
+#OK button
+OKbutton = Button(master, text="OK", command=ok)
+OKbutton.pack(pady=20, padx=20)
+OKbutton.config(height=1, width=8)
+OKbutton.place(relx=0.42, rely=0.08, anchor=N)
+
+#Clear button
+Clearbutton = Button(master, text="CLEAR", command=clear)
+Clearbutton.pack(pady=20 ,padx=20)
+Clearbutton.config(height=1, width=8)
+Clearbutton.place(relx=0.55, rely=0.08, anchor=N)
+
+#start ant loop through the program
+mainloop()
---
+++ methods.py
@@ -0,0 +1,221 @@
+import requests
+import json
+from collections import Counter
+import operator
+import itertools
+
+class methodsss:
+
+	def __init__():
+		pass
+
+
+	# publicly accessible list of top tens options
+	top_tens = ["Highest cost resources", "Lowest cost resources", "Most used resources", "Least used resources", "Highest cost apps", "Lowest cost apps", "Most used apps", "Least used apps"]
+
+	#send request of RESOURCES, process and sort alphabetically
+	def send_request_res():
+		url = "https://engineering-task.elancoapps.com/api/resources"
+		response_url = requests.get(url)
+		convert_to_text = response_url.text
+		convert_to_list = json.loads(convert_to_text)
+		sorted_list = sorted(convert_to_list, key=str.lower)
+		return sorted_list
+
+	#send request of APPLICATIONS, process and sort alphabetically
+	def send_request_apps():
+		url = "https://engineering-task.elancoapps.com/api/applications"
+		response_url = requests.get(url)
+		convert_to_text = response_url.text
+		convert_to_list = json.loads(convert_to_text)
+		sorted_list = sorted(convert_to_list, key=str.lower)
+		return sorted_list
+
+	#compute total cost of a list entity
+	def total_cost(some_list):
+		new_list = [i["Cost"] for i in some_list]
+		converted_to_f = [float(j) for j in new_list]
+		result = sum(converted_to_f)
+		return result
+
+	#compute average cost of a list entity
+	def average_cost(some_list):
+		lst_size = len(some_list)
+		result = methodsss.total_cost(some_list) / lst_size
+		return result
+
+	#compute total cost of a list entity
+	def time_frame(some_list):
+		result = []
+		new_list = [i["Date"] for i in some_list]
+		result.append(min(new_list))
+		result.append(max(new_list))
+		return result
+
+	#compute most popular location of a list entity
+	def location_counter(some_list):
+		new_list = [i["Location"] for i in some_list]
+		words = str([word for word, word_count in Counter(new_list).most_common(1)])
+		result = words.replace('[','').replace("'",'').replace("]",'')
+		return result
+
+	#compute top or bottom APPLICATIONS
+	def top_applications(some_list, var):
+		complete = ""
+		top_ten_cost = {}
+		dictlist = []
+		for i in some_list:
+			url = "https://engineering-task.elancoapps.com/api/applications/" + i
+			response_url = requests.get(url)
+			convert_to_text = response_url.text
+			convert_to_list = json.loads(convert_to_text)
+
+			if var == methodsss.top_tens[6] or var == methodsss.top_tens[7]:
+
+				new_list = [i["ConsumedQuantity"] for i in convert_to_list]
+				converted_to_f = [float(j) for j in new_list]
+				result = sum(converted_to_f)
+
+				top_ten_cost[i] = result
+
+			else:
+				new_list = [i["Cost"] for i in convert_to_list]
+				converted_to_f = [float(j) for j in new_list]
+				result = sum(converted_to_f)
+
+				top_ten_cost[i] = result
+
+		if var == methodsss.top_tens[4] or var == methodsss.top_tens[6]:
+
+			sorted_d = dict( sorted(top_ten_cost.items(), key=operator.itemgetter(1),reverse=True))
+			
+			temp = []
+			for key, value in sorted_d.items():
+				temp = [key, value]
+				dictlist.append(temp)
+				
+
+		elif var == methodsss.top_tens[5] or var == methodsss.top_tens[7]:
+			sorted_d = dict( sorted(top_ten_cost.items(), key=operator.itemgetter(1)))
+			#dictlist = []
+			temp = []
+			for key, value in sorted_d.items():
+				temp = [key, value]
+				dictlist.append(temp)
+				
+
+		
+
+		result = methodsss.convert_dict_to_list(dictlist, var)
+
+		return result
+
+	# convert a dictionary to list
+	def convert_dict_to_list(some_dict, var):
+		complete = ""
+		x=0
+		while x < 10:
+			temp = str(some_dict[x])
+			
+			temp2 = temp.replace('[',"").replace(']',"").replace("'","")
+			temp3 = temp2.split(", ")
+			if var == methodsss.top_tens[0] or  var == methodsss.top_tens[1] or var == methodsss.top_tens[4] or  var == methodsss.top_tens[5]:
+				complete = complete + str(x+1) + ": " + temp3[0] + " - Cost: " + temp3[1] +"\n"
+				x+=1
+			else:
+				complete = complete + str(x+1) + ": " + temp3[0] + " - Consumed quantity: " + temp3[1] +"\n"
+				x+=1
+
+		return complete
+
+	#compute top or bottom RESOURCES
+	def top_resources(some_list, var):
+		complete = ""
+		top_ten_cost = {}
+		dictlist = []
+		for i in some_list:
+			url = "https://engineering-task.elancoapps.com/api/resources/" + i
+			response_url = requests.get(url)
+			convert_to_text = response_url.text
+			convert_to_list = json.loads(convert_to_text)
+
+			if var == methodsss.top_tens[2] or var == methodsss.top_tens[3]:
+
+				new_list = [i["ConsumedQuantity"] for i in convert_to_list]
+				converted_to_f = [float(j) for j in new_list]
+				result = sum(converted_to_f)
+
+				top_ten_cost[i] = result
+
+			else:
+				new_list = [i["Cost"] for i in convert_to_list]
+				converted_to_f = [float(j) for j in new_list]
+				result = sum(converted_to_f)
+
+				top_ten_cost[i] = result
+
+		if var == methodsss.top_tens[0] or var == methodsss.top_tens[2]:
+
+			sorted_d = dict( sorted(top_ten_cost.items(), key=operator.itemgetter(1),reverse=True))
+			
+			temp = []
+			for key, value in sorted_d.items():
+				temp = [key, value]
+				dictlist.append(temp)
+				
+
+		elif var == methodsss.top_tens[1] or var == methodsss.top_tens[3]:
+			sorted_d = dict( sorted(top_ten_cost.items(), key=operator.itemgetter(1)))
+			#dictlist = []
+			temp = []
+			for key, value in sorted_d.items():
+				temp = [key, value]
+				dictlist.append(temp)
+				
+
+		
+
+		result = methodsss.convert_dict_to_list(dictlist, var)
+
+		return result
+
+		
+
+		
+
+		#RENAME
+
+	#determine which top tens option was selected
+	def which_top_selection(var):
+
+
+		if var == methodsss.top_tens[0] or var == methodsss.top_tens[1] or var == methodsss.top_tens[2] or var == methodsss.top_tens[3]:
+
+			convert_to_list = methodsss.send_request_res()
+
+			if var == methodsss.top_tens[0]:
+				result = methodsss.top_resources(convert_to_list, var)
+			elif var == methodsss.top_tens[1]:
+				result = methodsss.top_resources(convert_to_list,var)
+			elif var == methodsss.top_tens[2]:
+				result = methodsss.top_resources(convert_to_list,var)
+			elif var == methodsss.top_tens[3]:
+				result = methodsss.top_resources(convert_to_list,var)
+
+		else:
+			convert_to_list = methodsss.send_request_apps()
+			if var == methodsss.top_tens[4]:
+				result = methodsss.top_applications(convert_to_list, var)
+			elif var == methodsss.top_tens[5]:
+				result = methodsss.top_applications(convert_to_list,var)
+			elif var == methodsss.top_tens[6]:
+				result = methodsss.top_applications(convert_to_list,var)
+			elif var == methodsss.top_tens[7]:
+				result = methodsss.top_applications(convert_to_list,var)
+
+
+		return result
+			
+	
+
+	
